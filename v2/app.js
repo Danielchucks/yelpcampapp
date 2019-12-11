430a4e969cabed1b376634b675760469ab45229d
Danny King <shadowshadon@gmail.com.com>
2019-12-11 21:20:19 +0100

    Updating and adding of view folder

--- v2/app.js
+++ v2/app.js
@@ -16,33 +16,33 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create({
-   name: "Us Park", 
-   image: "https://live.staticflickr.com/4567/37514240304_1a744f1fce_z.jpg"
-}, function (err, campground) {
-   if(err){
-      console.log(err)
-   }else{
-      console.log("NEWLY CREATED CAMPGROUND ");
-      console.log(campground)
-   }
-});
+// Campground.create({
+//    name: "Us Park", 
+//    image: "https://live.staticflickr.com/4567/37514240304_1a744f1fce_z.jpg"
+// }, function (err, campground) {
+//    if(err){
+//       console.log(err)
+//    }else{
+//       console.log("NEWLY CREATED CAMPGROUND ");
+//       console.log(campground)
+//    }
+// });
 
-
-// The array is now accessible by all route
-var campground = [
-    {title: "Forest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuLcubrLpDuQMBbPsVHEiDiN8R9FlZ7m0wb-N1tv9dXPoEKRN4&s"},
-    {title: "Mount Rainier National Park ", image: "https://www.nps.gov/mora/planyourvisit/images/OhanaCampground2016_CMeleedy_01_web.jpeg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    {title: "Learnx", image: "https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722b7bd7964dc65b_340.jpg"},
-    {title: "Natural Beauty", image: "https://pixabay.com/get/57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722b7bd7964dc65b_340.jpg"}
-]
 
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds: campground});
+   // Get campground from the DB
+   Campground.find({}, function (err, allCampgrounds) {
+      if (err){
+         console.log(err)
+      } else{
+        res.render("campgrounds", {campgrounds: allCampgrounds})
+      }
+   });
+   //res.render("campgrounds", {campgrounds: campground});
 });
 
 app.post("/campgrounds", function (req, res) {
